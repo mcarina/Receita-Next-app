171d9e43c44dc7ddb911db95ed626737349ba287
mcarina <marciacarina931@gmail.com>
2024-12-12 10:10:28 -0400

    sem github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: 'standalone'
-// };
-
 const nextConfig = {
-    output: 'export',
-    basePath: '/Receita-Next-app',
-    assetPrefix: '/Receita-Next-app/',
+    output: 'standalone'
 };
-
-module.exports = {
-    assetPrefix: './',
-    basePath: '/Receita-Next-app',
-    trailingSlash: true,
-}
 
 export default nextConfig;
